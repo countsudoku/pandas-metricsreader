fba670400039424e68fe99340091ca1bf2c364cf
Moritz C.K.U. Schneider <schneider.moritz@googlemail.com>
2017-05-31 23:50:15 +0200

    Add intersphinx target

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -161,4 +161,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+        'python': ('https://docs.python.org/', None),
+        'requests': ('http://docs.python-requests.org/en/master/', None),
+}
