1057068ec70825d2af8aac77a93ff1f8f9cb8c6b
Moritz C.K.U. Schneider <schneider.moritz@googlemail.com>
2017-07-20 00:31:40 +0200

    Fix setup.py dependency cycle
    The __init__.py can't be imported if the dependencies of this package aren't
    installed. But they can't be installed if the module cant load setup.py. So we
    got a dependencie cycle. All this is related to the version number, which is
    loaded from __init__.
    Hence we now parse the file only for the version string. So we must not load
    the imports in __init__

--- setup.py
+++ setup.py
@@ -2,13 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+from ast import parse
+import os
 
-from pandas_metricsreader import __version__
+
+def version():
+    """Return version string."""
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                           'pandas_metricsreader',
+                           '__init__.py')) as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return parse(line).body[0].value.s
 
 setup(
     name='pandas-metricsreader',
     packages=find_packages(exclude=['docs', 'tests*']),
-    version=__version__,
+    version=version(),
     description='Read data from different monitoring systems into a pandas DataFrame',
     author='Moritz C.K.U. Schneider',
     author_email='schneider.moritz@gmail.com',
