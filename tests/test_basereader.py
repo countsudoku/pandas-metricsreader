c9c5d860fa2a8899756cfd1173f0ac43623aaa39
Moritz C.K.U. Schneider <countsudoku@users.noreply.github.com>
2017-11-29 17:46:32 +0100

    Styling
    * added basic docstrings to the test cases
    * renamed some mock objects from 'mock' to 'rmock' because they interfere with
      the mock package

--- tests/test_basereader.py
+++ tests/test_basereader.py
@@ -1,35 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pandas_metricsreader.BaseReader import BaseReader, MetricsReaderError
+""" Unit tests for the BaseReader class """
+
 import requests
 import requests_mock
 import pytest
 import mock
 
+from pandas_metricsreader.BaseReader import BaseReader, MetricsReaderError
+
 test_url = 'https://example.net'
 
-@pytest.fixture
+@pytest.fixture()
 def sample_reader():
+    """ Most basic BaseReader object """
     return BaseReader(url=test_url)
 
 def test_basereader_without_session_without_url_except_raising_exception():
+    """ Test if BaseReader raises exception, if created without url """
     with pytest.raises(Exception):
         BaseReader()
 
 def test_basereader_without_session_with_url(sample_reader):
+    """ Test if BaseReader has correct url attribute """
     assert sample_reader.url == test_url
 
 def test_basereader_get_without_session(sample_reader):
-    with requests_mock.Mocker() as mock:
-        mock.get(test_url, text='foobar')
+    """ Test functionality of a _get request with session created per default
+    """
+    with requests_mock.Mocker() as rmock:
+        rmock.get(test_url, text='foobar')
         r = sample_reader._get(test_url, {})
     assert isinstance(r, requests.models.Response)
     assert r.text == 'foobar'
-    assert mock.called
-    assert mock.call_count == 1
+    assert rmock.called
+    assert rmock.call_count == 1
 
 def test_basereader_get_with_session():
+    """ Test functionality of a _get request, if you hand over a session
+    object to the BaseReader """
     get = mock.Mock(name='get')
     get.return_value.status_code = 200
     session = type('', (), {})
@@ -45,7 +55,9 @@ def test_basereader_get_with_session():
     get.assert_called_once_with(test_url, params={}, timeout=13, verify=False)
 
 def test_basereader_get_to_wrong_url(sample_reader):
-    with requests_mock.Mocker() as mock:
-        mock.get(test_url, text='foobar', status_code='400')
+    """  Tests if a unauthorized request to a url, which needs authorization,
+    fails """
+    with requests_mock.Mocker() as rmock:
+        rmock.get(test_url, text='foobar', status_code='400')
         with pytest.raises(MetricsReaderError):
             sample_reader._get(test_url, {})
