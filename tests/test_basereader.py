913025fb9edad4fc2353c267f1c32b6f62a0d51b
Moritz C.K.U. Schneider <countsudoku@users.noreply.github.com>
2017-11-29 02:09:11 +0100

    Restore python2/3 compatibility in the tests

--- tests/test_basereader.py
+++ tests/test_basereader.py
@@ -20,11 +20,10 @@ def test_basereader_without_session_without_url_except_raising_exception():
 def test_basereader_without_session_with_url(sample_reader):
     assert sample_reader.url == test_url
 
-@requests_mock.Mocker(kw='mock')
-def test_basereader_get_without_session(sample_reader, **kw):
-    mock = kw['mock']
-    mock.get(test_url, text='foobar')
-    r = sample_reader._get(test_url, {})
+def test_basereader_get_without_session(sample_reader):
+    with requests_mock.Mocker() as mock:
+        mock.get(test_url, text='foobar')
+        r = sample_reader._get(test_url, {})
     assert isinstance(r, requests.models.Response)
     assert r.text == 'foobar'
     assert mock.called
@@ -45,9 +44,8 @@ def test_basereader_get_with_session():
     r = reader._get(test_url, {})
     get.assert_called_once_with(test_url, params={}, timeout=13, verify=False)
 
-@requests_mock.Mocker(kw='mock')
-def test_basereader_get_to_wrong_url(sample_reader, **kw):
-    mock = kw['mock']
-    mock.get(test_url, text='foobar', status_code='400')
-    with pytest.raises(MetricsReaderError):
-        sample_reader._get(test_url, {})
+def test_basereader_get_to_wrong_url(sample_reader):
+    with requests_mock.Mocker() as mock:
+        mock.get(test_url, text='foobar', status_code='400')
+        with pytest.raises(MetricsReaderError):
+            sample_reader._get(test_url, {})
